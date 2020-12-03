1bfb977cff8f19de24455c0eaf1db3246467d027
github username <github email address>
2020-12-03 01:02:57 -0600

    commit tree 0177297308e60e1253caf24e1c08775044d66f5b

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
 console.log('%c HI', 'color: firebrick')
+
+function fetchImg() {
+    const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+
+    fetch(img)
+    .then(resp => resp.json())
+    .then(json => renderImg(json))
+}
